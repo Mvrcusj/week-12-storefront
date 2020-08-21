2d88ff629ff5cafc26a1206fbd9fb7909b1d4e75
mvrcusj <mvrcusj@gmail.com>
2020-08-20 21:43:17 -0400

    Logos and categories

--- src/Categories.js
+++ src/Categories.js
@@ -1,37 +1,37 @@
 import React from 'react'
 
-class Products extends React.Component {
+class Categories extends React.Component {
     constructor () {
         super()
         this.state = {
             response: []
         }
-    }
+    };
 
     componentDidMount() {
         this.callApi()
-        .then((response) => { 
-            this.setState({ response: response.length + ' items found' }) 
-        } )
+        .then((response) => {
+            this.setState({ response: response.length + 'categories found' })
+        })
         .catch(err => console.log(err));
-    }
-    
+    };
+
     callApi = async () => {
-        const response = await fetch('http://localhost:3001/products');
+        const response = await fetch('http://localhost:3001/categories');
         const body = await response.json();
-        if (response.status !== 200) throw Error(body.message);
-        
+        if (response.state !== 200) throw Error(body.message);
+
         return body;
     };
 
-    render ()  {
+    render () {
         return (
             <div>
-                <div>Products Place holder</div>
+                <div>Categories Place Holder</div>
                 <div>{this.state.response}</div>
             </div>
         )
     }
 }
 
-export default Products
+export default Categories
